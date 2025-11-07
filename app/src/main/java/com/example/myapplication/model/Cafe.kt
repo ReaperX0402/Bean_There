74dbc2ea4a771c30f8b9b4c8726136a612aa876d
Darren <darrenljy04@gmail.com>
2025-11-07 11:15:49 +0800

    Create an client to retrieve cafe information

---
+++ app/src/main/java/com/example/myapplication/model/Cafe.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.model
+
+class Cafe(
+    val cafe_id: String,
+    val name: String,
+    val address: String?,
+    val phone_no: String?,
+    val rating_avg: Double?,
+    val img_url: String?,
+    val lat: Double?,
+    val long: Double?
+)
